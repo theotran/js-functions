68e57a41b0e57d490c04378bb1cf760a43ed403f
theo tran <theotran@rocketmail.com>
2015-11-05 19:06:45 -1000

    Passed all tests

--- functions.js
+++ functions.js
@@ -4,13 +4,13 @@
  * @return {string} the number as a string
  */
 var num = 15;
- function convert (number) {
+ function numberToString (number) {
  	var n = number.toString();
    return n;
 
  }
 
-console.log(convert(num));
+
 
 /**
  * Adds one to a given number.
@@ -18,7 +18,7 @@ console.log(convert(num));
  * @return {number}
  */
 
-function addOne (number) {
+function increase (number) {
   var added = number +1;
   return added;
 }
@@ -29,7 +29,7 @@ function addOne (number) {
  * @param {number} n
  * @return {number}
  */
-function subtractOne (number) {
+function decrease (number) {
   var added = number -1;
   return added;
 }
@@ -41,7 +41,7 @@ function subtractOne (number) {
  * @return {number} the sum
  */
 
-function addition (x, y) {
+function add (x, y) {
   var sum = x + y;
   return sum;
 }
@@ -54,7 +54,7 @@ function addition (x, y) {
  * @return {number} the difference
  */
 
-function subtraction (x, y) {
+function subtract (x, y) {
   var difference = x - y;
   return difference;
 }
@@ -67,7 +67,7 @@ function subtraction (x, y) {
  * @return {number} the product
  */
 
-function multiplication (x, y) {
+function multiply (x, y) {
   var product = x * y;
   return product;
 }
@@ -79,7 +79,7 @@ function multiplication (x, y) {
  * @return {number} the quotient
  */
 
-function division (x, y) {
+function divide (x, y) {
   var total = x / y;
   return total;
 }
@@ -90,7 +90,10 @@ function division (x, y) {
  * @return {number} squared
  */
 
-
+function square (x) {
+  var squared = x * x;
+  return squared;
+}
 /**
  * Performs a mathematical operation on two numbers.
  * Also prints out the equation: (i.e.) "1 + 5 = 6" or "8 / 2 = 4".
@@ -99,15 +102,40 @@ function division (x, y) {
  * @param {number} y
  * @return {number} the result
  */
+function calculate (operation, x, y) {
+  var result = 0;
+    if (operation === "add") {
+      result = x + y;
+      console.log( x + " + " + y + " = " + result);
+      return result;
+  }
+    if (operation === "subtract") {
+      result = x - y;
+      console.log( x + " - " + y + " = " + result);
+      return result;
+  }
+    if (operation === "multiply") {
+      result = x * y;
+      console.log( x + " * " + y + " = " + result);
+      return result;
+  }
+    if (operation === "divide") {
+      result = x / y;
+      console.log( x + " / " + y + " = " + result);
+      return result;
+  }
 
 
+}
 /**
  * Returns true if `a` is greater than `b`.
  * @param {number} a
  * @param {number} b
  * @return {boolean} `a` is larger than `b`
  */
-
+function isGreaterThan (a, b) {
+  return a > b;
+}
 
 /**
  * Returns true if `a` is less than `b`.
@@ -116,6 +144,9 @@ function division (x, y) {
  * @return {boolean} `a` is smaller than `b`
  */
 
+function isLessThan (a, b) {
+  return a < b;
+}
 
 /**
  * Returns true if `a` and `b` are equal.
@@ -123,6 +154,9 @@ function division (x, y) {
  * @param {number} b
  * @return {boolean} the numbers are equal
  */
+function areEqual (a, b) {
+  return a === b;
+}
 
 
 /**
@@ -131,7 +165,13 @@ function division (x, y) {
  * @param {number} y
  * @return {number} the smallest number
  */
-
+function minimum (x, y) {
+  if (x < y) {
+    return x;
+  } else {
+    return y;
+  }
+}
 
 /**
  * Returns the largest value of two numbers.
@@ -140,20 +180,38 @@ function division (x, y) {
  * @return {number} the largest number
  */
 
-
+function maximum (x, y) {
+  if (x > y) {
+    return x;
+  } else {
+    return y;
+  }
+}
 /**
  * Returns true if `n` is even.
  * @param {number} n
  * @return {boolean} the number is even
  */
 
-
+function isEven (n) {
+  if (n % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 /**
  * Returns true if `n` is odd.
  * @param {number} n
  * @return {boolean} the number is odd
  */
-
+function isOdd (n) {
+  if (n % 2 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /**
  * Returns a letter grade.
@@ -166,7 +224,24 @@ function division (x, y) {
  * @param {number} total maximum possible score
  * @return {string} the score represented as a letter grade
  */
-
+function letterGrade (score, total) {
+  var max = 100;
+  if (score/total >= 0.9){
+    return "A";
+  }
+  if (score/total >=0.8) {
+    return "B";
+  }
+  if (score/total >= 0.7) {
+    return "C";
+  }
+  if (score/total >= 0.6) {
+    return "D";
+  }
+  if (score/total >= 0) {
+    return "F";
+  }
+}
 
 /**
  * Checks if a `restaurant` object has a `reviews` property.
@@ -176,14 +251,29 @@ function division (x, y) {
  * @return {object} restaurant
  */
 
+var restaurant = {
+  reviews: 1
+};
 
+function incrementReviews (restaurant) {
+   
+    if (restaurant.reviews > 0) {
+      restaurant.reviews ++;
+    } else {
+      restaurant.reviews = 1;
+    }
+    return restaurant;
+  
+}
 /**
  * Joins two strings with a space.
  * @param {string} word1
  * @param {string} word2
  * @return {string} joined the words joined with a space
  */
-
+function combine (word1, word2) {
+  return word1 + " " + word2;
+}
 
 /**
  * Returns a circle object with the properties `circumference` and `area`.
@@ -192,4 +282,14 @@ function division (x, y) {
  * @param {number} radius
  * @return {object} circle
  */
+
+ function createCircle(radius) {
+    var circle = {
+      circumference: (2 * Math.PI * radius),
+      area: (Math.PI * radius * radius)
+      
+    };
+
+    return circle;
+ }
 
